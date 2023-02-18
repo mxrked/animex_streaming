f5b49aa0e45aaef2233e8f6bf85dc5dd15e8fbe5
Parker <pp101socials@gmail.com>
2023-02-18 09:24:54 -0500

    Added main and PageHead

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ import { useRouter } from "next/router";
 // Data/Functions/Images Imports
 
 // Component Imports
+import { PageHead } from "@/assets/components/global/PageHead";
 
 // Style Imports
 import "../assets/styles/modules/Index/Index.module.css";
@@ -20,5 +21,11 @@ import "../assets/styles/modules/Index/Index.module.css";
 export default function Home() {
   const router = useRouter();
 
-  return <div id="page" className="page overrides_Index full-second"></div>;
+  return <div id="page" className="page overrides_Index full-second">
+  
+    <PageHead title="Animex Streaming - Home" keywords="" description="Looking for an anime streaming website that has all your favorite shows in one place? Look no further than Animex Streaming! Our vast collection of anime includes classic favorites and new releases, all available to stream in high quality. With easy-to-use navigation and a user-friendly interface, Animex Streaming is the perfect destination for anime fans of all ages. Sign up now and start watching your favorite shows today!" robots="index, follow" url="https://sparkling-dragon-f3af91.netlify.app/"/>
+
+    <main id="pageCnt" className="page-cnt full-second"></main>
+  
+  </div>;
 }
