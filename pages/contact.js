f35f1584b5bcd3a6c09fbbeaa53e91176d1e56ac
Parker <pp101socials@gmail.com>
2023-02-18 09:30:27 -0500

    Added main and PageHead

--- pages/contact.js
+++ pages/contact.js
@@ -13,6 +13,7 @@ import { useRouter } from "next/router";
 // Data/Functions/Images Imports
 
 // Component Imports
+import { PageHead } from "@/assets/components/global/PageHead";
 
 // Style Imports
 // import "../assets/styles/modules/Contact/Contact.module.css";
@@ -20,5 +21,17 @@ import { useRouter } from "next/router";
 export default function Contact() {
   const router = useRouter();
 
-  return <div id="page" className="page overrides_Contact full-second"></div>;
+  return (
+    <div id="page" className="page overrides_Contact full-second">
+      <PageHead
+        title="Animex Streaming - Contact/Support"
+        keywords=""
+        description="Looking for an anime streaming website that has all your favorite shows in one place? Look no further than Animex Streaming! Our vast collection of anime includes classic favorites and new releases, all available to stream in high quality. With easy-to-use navigation and a user-friendly interface, Animex Streaming is the perfect destination for anime fans of all ages. Sign up now and start watching your favorite shows today!"
+        robots="index, follow"
+        url="https://sparkling-dragon-f3af91.netlify.app/contact/"
+      />
+
+      <main id="pageCnt" className="page-cnt full-second"></main>
+    </div>
+  );
 }
--- pages/login_register.js
+++ pages/login_register.js
@@ -13,6 +13,7 @@ import { useRouter } from "next/router";
 // Data/Functions/Images Imports
 
 // Component Imports
+import { PageHead } from "@/assets/components/global/PageHead";
 
 // Style Imports
 // import "../assets/styles/modules/Login_Register/Login_Register.module.css";
@@ -21,6 +22,16 @@ export default function Login_Register() {
   const router = useRouter();
 
   return (
-    <div id="page" className="page overrides_Login_Register full-second"></div>
+    <div id="page" className="page overrides_Login_Register full-second">
+      <PageHead
+        title="Animex Streaming - Login/Register"
+        keywords=""
+        description="Looking for an anime streaming website that has all your favorite shows in one place? Look no further than Animex Streaming! Our vast collection of anime includes classic favorites and new releases, all available to stream in high quality. With easy-to-use navigation and a user-friendly interface, Animex Streaming is the perfect destination for anime fans of all ages. Sign up now and start watching your favorite shows today!"
+        robots="index, follow"
+        url="https://sparkling-dragon-f3af91.netlify.app/login_register/"
+      />
+
+      <main id="pageCnt" className="page-cnt full-second"></main>
+    </div>
   );
 }
