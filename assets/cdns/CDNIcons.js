4aa419facf0af0024e84db43945a1f8c8ca3a7f8
Parker <pp101socials@gmail.com>
2023-02-18 11:44:46 -0500

    Added logo icons

--- assets/cdns/CDNIcons.js
+++ assets/cdns/CDNIcons.js
@@ -12,5 +12,6 @@ const F16 = CDNIconReturn("tab-icons", "favicon-16x16", "png");
 const F32 = CDNIconReturn("tab-icons", "favicon-32x32", "png");
 
 const LOGO_LARGE_RED = CDNIconReturn("logo", "logo-large-red", "webp");
+const LOGO_LARGE_ORANGE = CDNIconReturn("logo", "logo-large-orange", "webp");
 
-export { ATI, FAVICON, F16, F32, LOGO_LARGE_RED };
+export { ATI, FAVICON, F16, F32, LOGO_LARGE_ORANGE, LOGO_LARGE_RED };
