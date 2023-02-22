1629d2a33d000809940e713e346f6bcd5463b7f7
Parker <pp101socials@gmail.com>
2023-02-22 15:59:46 -0500

    Added cluster images

--- assets/cdns/CDNImgs.js
+++ assets/cdns/CDNImgs.js
@@ -6,6 +6,12 @@
 
 import { CDNImgReturn } from "./CDNReturns";
 
+const CLUSTER_1 = CDNImgReturn("clusters", "cluster-1", "webp");
+const CLUSTER_2 = CDNImgReturn("clusters", "cluster-2", "webp");
+const CLUSTER_3 = CDNImgReturn("clusters", "cluster-3", "webp");
+const CLUSTER_4 = CDNImgReturn("clusters", "cluster-4", "webp");
+const CLUSTER_5 = CDNImgReturn("clusters", "cluster-5", "webp");
+
 const AFRESHPLACE_IMG = CDNImgReturn("projects", "afreshplace-display", "webp");
 const FAUNAANDFLORA_IMG = CDNImgReturn(
   "projects",
@@ -15,6 +21,8 @@ const FAUNAANDFLORA_IMG = CDNImgReturn(
 const ONLYJOJO_IMG = CDNImgReturn("projects", "onlyjojo-display", "webp");
 const RTSYVISUALS_IMG = CDNImgReturn("projects", "rtsyvisuals-display", "webp");
 const SHARPQUILLS_IMG = CDNImgReturn("projects", "sharpquills-display", "webp");
+
+export { CLUSTER_1, CLUSTER_2, CLUSTER_3, CLUSTER_4, CLUSTER_5 };
 
 export {
   AFRESHPLACE_IMG,
