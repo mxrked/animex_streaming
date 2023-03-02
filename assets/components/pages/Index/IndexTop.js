e82fc883b9ca88163ed95a7860977e75f6a7991d
Parker <pp101socials@gmail.com>
2023-03-02 07:47:55 -0500

    Removed animation holder

--- assets/components/pages/Index/IndexTop.js
+++ assets/components/pages/Index/IndexTop.js
@@ -23,50 +23,44 @@ export const IndexTop = () => {
 
   return (
     <section id="indexTop" className={`${styles.index_top}`}>
-      <div
-        className="animation-holder full-second"
-        id="indexTopAnimationHolder"
-      >
-        <BackgroundImage src={TOP_BG} className={`${styles.bg}`} />
+      <BackgroundImage src={TOP_BG} className={`${styles.bg}`} />
 
-        <div className={`${styles.hover_trigger}`}>
-          <div className={`${styles.index_top_inner}`}>
-            <div className={`${styles.index_top_inner_box} container-fluid`}>
-              <div className={`${styles.index_top_inner_row} row`}>
+      <div className={`${styles.hover_trigger}`}>
+        <div className={`${styles.index_top_inner}`}>
+          <div className={`${styles.index_top_inner_box} container-fluid`}>
+            <div className={`${styles.index_top_inner_row} row`}>
+              <div
+                className={`${styles.index_top_inner_side} ${styles.index_top_inner_L} col-lg-6 col-md-6 col-sm-6 col-xs-6`}
+              >
                 <div
-                  className={`${styles.index_top_inner_side} ${styles.index_top_inner_L} col-lg-6 col-md-6 col-sm-6 col-xs-6`}
+                  className={`${styles.index_top_inner_side_cnt} animate-fade-down-sub full-second`}
                 >
+                  <h1 className="half-second">Anime isn't just art.</h1>
+                  <h1 className="half-second">It's an experience.</h1>
+
+                  <p className="half-second">
+                    Browse the different series we have available for streaming.
+                  </p>
+
                   <div
-                    className={`${styles.index_top_inner_side_cnt} animate-fade-down-sub full-second`}
+                    className="half-second"
+                    onClick={() => {
+                      ExitAndRoute(router, "/series", seriesRouted);
+                    }}
                   >
-                    <h1 className="half-second">Anime isn't just art.</h1>
-                    <h1 className="half-second">It's an experience.</h1>
-
-                    <p className="half-second">
-                      Browse the different series we have available for
-                      streaming.
-                    </p>
-
-                    <div
-                      className="half-second"
-                      onClick={() => {
-                        ExitAndRoute(router, "/series", seriesRouted);
-                      }}
-                    >
-                      <span className="half-second">BROWSE</span>
-                    </div>
+                    <span className="half-second">BROWSE</span>
                   </div>
                 </div>
-                <div
-                  className={`${styles.index_top_inner_side} ${styles.index_top_inner_R} col-lg-6 col-md-6 col-sm-6 col-xs-6`}
-                >
-                  <div className={`${styles.index_top_inner_side_cnt}`}>
-                    <img
-                      data-src={CLUSTER_1}
-                      className="lazyload animate-fade-down-sub full-second"
-                      alt="Image of Tanjiro, Deku, Eren and Jotaro."
-                    />
-                  </div>
+              </div>
+              <div
+                className={`${styles.index_top_inner_side} ${styles.index_top_inner_R} col-lg-6 col-md-6 col-sm-6 col-xs-6`}
+              >
+                <div className={`${styles.index_top_inner_side_cnt}`}>
+                  <img
+                    data-src={CLUSTER_1}
+                    className="lazyload animate-fade-down-sub full-second"
+                    alt="Image of Tanjiro, Deku, Eren and Jotaro."
+                  />
                 </div>
               </div>
             </div>
