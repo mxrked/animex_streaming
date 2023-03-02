651d94657c715af4cbdd199c277467d96c0f8c55
Parker <pp101socials@gmail.com>
2023-03-02 07:56:38 -0500

    Created anims

---
+++ assets/anims/FADES.js
@@ -0,0 +1,17 @@
+/**
+ *
+ *  This is the fades for the Framer Motion
+ *
+ */
+
+const FADE_IN = {
+  visible: {
+    opacity: 1,
+    transition: { duration: 1 },
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+
+export { FADE_IN };
