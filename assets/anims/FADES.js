819ef322902e2e4c468360fd8a330437ed0e3539
Parker <pp101socials@gmail.com>
2023-03-05 09:18:03 -0500

    Added FADE_LEFT and FADE_RIGHT

--- assets/anims/FADES.js
+++ assets/anims/FADES.js
@@ -14,6 +14,29 @@ const FADE_IN = {
   },
 };
 
+const FADE_LEFT = {
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.4 },
+  },
+  hidden: {
+    opacity: 0,
+    x: 50,
+  },
+};
+
+const FADE_RIGHT = {
+  visible: {
+    opacity: 1,
+    x: 0,
+  },
+  hidden: {
+    opacity: 0,
+    x: -50,
+  },
+};
+
 const FADE_UP = {
   visible: {
     opacity: 1,
@@ -26,4 +49,4 @@ const FADE_UP = {
   },
 };
 
-export { FADE_IN, FADE_UP };
+export { FADE_IN, FADE_UP, FADE_LEFT, FADE_RIGHT };
