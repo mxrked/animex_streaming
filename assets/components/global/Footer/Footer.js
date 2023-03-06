6d25c92ce5120cbc4bb14cd385466376f228f98e
Parker <pp101socials@gmail.com>
2023-03-06 11:35:29 -0500

    Created file

---
+++ assets/components/global/Footer/Footer.js
@@ -0,0 +1,15 @@
+/**
+ *
+ *  This is the footer
+ *
+ */
+
+import { FaTwitter, FaInstagram, FaFacebook } from "react-icons/fa";
+
+import ExitAndRoute from "@/assets/functions/routing/ExitAndRoute";
+
+import styles from "../../../styles/modules/Footer/Footer.module.css";
+
+export const Footer = () => {
+  return <footer id="footer" className={`${styles.footer}`}></footer>;
+};
