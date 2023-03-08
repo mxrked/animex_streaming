12d08ae465a07a394e79a227508ec16517b4397f
Parker <pp101socials@gmail.com>
2023-03-08 05:52:34 -0500

    Added components

--- pages/login_register.js
+++ pages/login_register.js
@@ -15,7 +15,13 @@ import { useRouter } from "next/router";
 // Component Imports
 import { PageHead } from "@/assets/components/global/PageHead";
 
+import { LoginRegisterBG } from "@/assets/components/pages/Login_Register/LoginRegisterBG";
+import { LoginRegisterTop } from "@/assets/components/pages/Login_Register/LoginRegisterTop";
+import { LoginRegisterForms } from "@/assets/components/pages/Login_Register/LoginRegisterForms";
+
 // Style Imports
+
+import pageStyles from "../assets/styles/modules/Login_Register/Login_Register.module.css";
 import "../assets/styles/modules/Login_Register/Login_Register.module.css";
 
 export default function Login_Register() {
@@ -30,7 +36,23 @@ export default function Login_Register() {
       />
 
       <main id="pageCnt" className="page-cnt full-second">
-        Login Register
+        <div className={`${pageStyles.page_cnt_box} container-fluid`}>
+          <div className={`${pageStyles.page_cnt_row} row`}>
+            <div
+              className={`${pageStyles.page_cnt_side} ${pageStyles.page_cnt_L} col-lg-4 col-md-4 col-sm-4 col-xs-12`}
+            >
+              <LoginRegisterBG />
+            </div>
+            <div
+              className={`${pageStyles.page_cnt_side} ${pageStyles.page_cnt_R} col-lg-8 col-md-8 col-sm-8 col-xs-12`}
+            >
+              <div className={`${pageStyles.page_cnt_side_cnt}`}>
+                <LoginRegisterTop />
+                <LoginRegisterForms />
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
