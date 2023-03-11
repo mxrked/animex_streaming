ffbca2cc600230c9b4ca9a7f53579dfdbec07530
Parker <pp101socials@gmail.com>
2023-03-11 09:54:18 -0500

    Created file

---
+++ assets/functions/dom/closers/CloseModal.js
@@ -0,0 +1,40 @@
+/**
+ *
+ *  This is used to close a modal
+ *
+ */
+
+import RemoveStorageVariable from "../../data/storage/RemoveStorageVariable";
+
+import { SERIES_ROUTES } from "@/assets/data/variables/ARRAYS";
+
+export default function CloseModal() {
+  // Removing hash from URL
+  for (let i = 0; i < SERIES_ROUTES.length; i++) {
+    if (window.location.hash == "#" + SERIES_ROUTES[i]) {
+      history.pushState(
+        "",
+        document.title,
+        window.location.pathname + window.location.search
+      );
+    }
+  }
+
+  // Hiding the modal(s)
+  document.querySelectorAll("._modal").forEach((modal) => {
+    modal.style.pointerEvents = "none";
+    modal.style.overflowY = "hidden";
+
+    setTimeout(() => {
+      modal.style.opacity = 0;
+      modal.style.visibility = "hidden";
+    }, 400);
+
+    setTimeout(() => {
+      RemoveStorageVariable("session", "Modal Opened");
+
+      document.body.style.overflowY = "auto";
+      document.body.style.pointerEvents = "auto";
+    }, 1400);
+  });
+}
