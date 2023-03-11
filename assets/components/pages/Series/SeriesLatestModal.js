deb051b0f6bc7f49b40e0a5d0aee0b1a9c6588a7
Parker <pp101socials@gmail.com>
2023-03-11 09:55:10 -0500

    Created file

---
+++ assets/components/pages/Series/SeriesLatestModal.js
@@ -0,0 +1,68 @@
+/**
+ *
+ *  This is the series latest modal
+ *
+ */
+
+import { BackgroundImage } from "react-image-and-background-image-fade";
+
+import { LATEST_SERIES } from "@/assets/data/variables/ARRAYS";
+
+import CloseModal from "@/assets/functions/dom/closers/CloseModal";
+
+import styles from "../../../styles/modules/Series/Series.module.css";
+
+export const SeriesLatestModal = () => {
+  return (
+    <div
+      id="latestPreferenceModal"
+      className={`${styles.modal} ${styles.latest_modal} _modal full-second`}
+    >
+      <div
+        className={`${styles.modal_darken}`}
+        onClick={() => {
+          CloseModal();
+        }}
+      ></div>
+      <div className={`${styles.modal_inner} _modal-inner`}>
+        <div className={`${styles.modal_inner_cnt}`}>
+          <div className={`${styles.modal_inner_cnt_top}`}>
+            <h1>Latest Series</h1>
+
+            <button
+              className={`${styles.closer} half-second`}
+              onClick={() => {
+                CloseModal();
+              }}
+            >
+              Close Latest
+            </button>
+          </div>
+
+          <div className={`${styles.modal_inner_cnt_box} container-fluid`}>
+            <div className={`${styles.modal_inner_cnt_row} row`}>
+              {LATEST_SERIES.map((series) => (
+                <div
+                  className={`${styles.series} col-lg-4 col-md-4 col-sm-4 col-xs-12`}
+                >
+                  <div className={`${styles.series_inner}`}>
+                    <BackgroundImage
+                      src={series.getImg}
+                      className={`${styles.bg} half-second`}
+                      width="100%"
+                      height="100%"
+                    />
+
+                    <button className={`${styles.darken} half-second`}>
+                      <span>{series.getName}</span>
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ assets/components/pages/Series/SeriesPopularModal.js
@@ -0,0 +1,68 @@
+/**
+ *
+ *  This is the series popular modal
+ *
+ */
+
+import { BackgroundImage } from "react-image-and-background-image-fade";
+
+import CloseModal from "@/assets/functions/dom/closers/CloseModal";
+
+import { POPULAR_SERIES } from "@/assets/data/variables/ARRAYS";
+
+import styles from "../../../styles/modules/Series/Series.module.css";
+
+export const SeriesPopularModal = () => {
+  return (
+    <div
+      id="popularPreferenceModal"
+      className={`${styles.modal} ${styles.popular_modal} _modal full-second`}
+    >
+      <div
+        className={`${styles.modal_darken}`}
+        onClick={() => {
+          CloseModal();
+        }}
+      ></div>
+      <div className={`${styles.modal_inner}`}>
+        <div className={`${styles.modal_inner_cnt}`}>
+          <div className={`${styles.modal_inner_cnt_top}`}>
+            <h1>Popular Series</h1>
+
+            <button
+              className={`${styles.closer} half-second`}
+              onClick={() => {
+                CloseModal();
+              }}
+            >
+              Close Popular
+            </button>
+          </div>
+
+          <div className={`${styles.modal_inner_cnt_box} container-fluid`}>
+            <div className={`${styles.modal_inner_cnt_row} row`}>
+              {POPULAR_SERIES.map((series) => (
+                <div
+                  className={`${styles.series} col-lg-4 col-md-4 col-sm-4 col-xs-12`}
+                >
+                  <div className={`${styles.series_inner}`}>
+                    <BackgroundImage
+                      src={series.getImg}
+                      className={`${styles.bg} half-second`}
+                      width="100%"
+                      height="100%"
+                    />
+
+                    <button className={`${styles.darken} half-second`}>
+                      <span>{series.getName}</span>
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
