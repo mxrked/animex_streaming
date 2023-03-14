a4ba1f39548218f897a134ba951137fafdf566ca
Parker <pp101socials@gmail.com>
2023-03-14 13:47:58 -0400

    Added videos

--- assets/data/classes/Series.js
+++ assets/data/classes/Series.js
@@ -4,7 +4,7 @@
  *
  */
 
-const {
+import {
   AOT_BG,
   CSM_BG,
   DEMON_SLAYER_BG,
@@ -14,14 +14,26 @@ const {
   OPM_BG,
   SXF_BG,
   VINLAND_SAGA_BG,
-} = require("@/assets/cdns/CDNBgs");
+} from "../../cdns/CDNBgs";
+import {
+  AOT_S1_TRAILER,
+  JOJO_TRAILER,
+  CSM_TRAILER,
+  DS_S1_TRAILER,
+  MHA_S1_TRAILER,
+  MP100_S1_TRAILER,
+  OPM_S1_TRAILER,
+  SXF_S1_TRAILER,
+  VLS_S1_TRAILER,
+} from "@/assets/cdns/CDNFiles";
 
 class Series {
-  constructor(name, desc, genre, img, release_date, link) {
+  constructor(name, desc, genre, img, video, release_date, link) {
     this.name = name;
     this.desc = desc;
     this.genre = genre;
     this.img = img;
+    this.video = video;
     this.release_date = release_date;
     this.link = link;
   }
@@ -37,6 +49,9 @@ class Series {
   }
   get getImg() {
     return this.img;
+  }
+  get getVideo() {
+    return this.video;
   }
   get getReleaseDate() {
     return this.release_date;
@@ -57,6 +72,9 @@ class Series {
   set setImg(img) {
     this.img = img;
   }
+  set setVideo(video) {
+    this.video = video;
+  }
   set setReleaseDate(release_date) {
     this.release_date = release_date;
   }
@@ -70,6 +88,7 @@ const AOT_OBJ = new Series(
   "People fight giant monsters known as 'Titans' as they try to survive behind tall stone walls.",
   "Shonen, Horror",
   AOT_BG,
+  AOT_S1_TRAILER,
   "2013",
   "#Attack_On_Titan"
 );
@@ -78,6 +97,7 @@ const CSM_OBJ = new Series(
   "A low on his life teen gains the ability to project chainsaws from his body and fight devils.",
   "Shonen, Horror, Romance",
   CSM_BG,
+  CSM_TRAILER,
   "2022",
   "#Chainsaw_Man"
 );
@@ -86,6 +106,7 @@ const DEMON_SLAYER_OBJ = new Series(
   "A boy and his demon sister work together to fight powerful demons.",
   "Shonen, Fantasy, Comedy",
   DEMON_SLAYER_BG,
+  DS_S1_TRAILER,
   "2021",
   "#Demon_Slayer"
 );
@@ -94,6 +115,7 @@ const JOJO_OBJ = new Series(
   "A tale of ancestory that involve alot of wacky and epic yet bizarre adventures.",
   "Shonen, Comedy, Fantasy",
   JOJO_BG,
+  JOJO_TRAILER,
   "2012",
   "#Jojos_Bizarre_Adventure"
 );
@@ -102,6 +124,7 @@ const MHA_OBJ = new Series(
   "A powerless boy tries his hardest to fit in and gain the powers of a superhero.",
   "Shonen, Comedy",
   MHA_BG,
+  MHA_S1_TRAILER,
   "2018",
   "#My_Hero_Academia"
 );
@@ -110,6 +133,7 @@ const MOB_PSYCHO_OBJ = new Series(
   "An unconfident middle-schooler exorcises ghosts and spirits in his city.",
   "Shonen, Comedy, Romance",
   MOB_PSYCHO_BG,
+  MP100_S1_TRAILER,
   "2018",
   "#Mob_Psycho_100"
 );
@@ -118,6 +142,7 @@ const OPM_OBJ = new Series(
   "An incredibly powerful superhero comes to the realization that being too powerful can be boring.",
   "Shonen, Comedy",
   OPM_BG,
+  OPM_S1_TRAILER,
   "2015",
   "#One_Punch_Man"
 );
@@ -126,6 +151,7 @@ const SXF_OBJ = new Series(
   "A spy and an assassin, become parents for a little girl who can read minds.",
   "Romance, Comedy",
   SXF_BG,
+  SXF_S1_TRAILER,
   "2022",
   "#Spy_X_Family"
 );
@@ -134,6 +160,7 @@ const VINLAND_SAGA_OBJ = new Series(
   "A young warrior tries to get revenge for the loss of someone who he loved dearly.",
   "Seinen, Fantasy",
   VINLAND_SAGA_BG,
+  VLS_S1_TRAILER,
   "2019",
   "#Vinland_Saga"
 );
